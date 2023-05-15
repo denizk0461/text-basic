62278bbfe035e8952ae47103e3d26fa63d013a84
denizk0461 <denizk0461@gmail.com>
2023-05-15 17:00:38 +0200

    Revamped the settings page in SettingsNewFragment.kt, but no functions are currently implemented

---
+++ app/src/main/java/com/denizd/textbasic/fragment/SettingsNewFragment.kt
@@ -0,0 +1,10 @@
+package com.denizd.textbasic.fragment
+
+import com.denizd.textbasic.R
+import com.denizd.textbasic.databinding.FragmentSettingsNewBinding
+import com.denizd.textbasic.util.viewBinding
+
+class SettingsNewFragment : BaseFragment(R.layout.fragment_settings_new) {
+
+    private val binding: FragmentSettingsNewBinding by viewBinding(FragmentSettingsNewBinding::bind)
+}
